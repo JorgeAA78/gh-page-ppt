788e6049701ee1f833f776a2d55f830245a8fa5e
Jorge Altamirano <grisjaa@gmail.com>
2025-10-05 13:59:23 -0300

    Updates

--- PPT.ccfc03dd.js
+++ PPT.ccfc03dd.js
@@ -1,4 +1,4 @@
-const e={data:{currentGame:{playerPlay:"",computerPlay:""},history:{player:0,computer:0}},listeners:[],init(){let e=localStorage.getItem("saved-state");e&&(this.data.history=JSON.parse(e))},getState(){return this.data},setState(e){for(let t of(this.data=e,this.listeners))t();localStorage.setItem("saved-state",JSON.stringify(this.data.history)),console.log("State updated:",this.data)},subscribe(e){this.listeners.push(e)},setPlayerMove(e){this.getState().currentGame.playerPlay=e,this.setComputerMove()},setComputerMove(){let e=["piedra","papel","tijera"][Math.floor(3*Math.random())],t=this.getState();t.currentGame.computerPlay=e,this.pushToHistory(t.currentGame.playerPlay,e)},whoWins:(e,t)=>e===t?"tie":"piedra"===e&&"tijera"===t||"papel"===e&&"piedra"===t||"tijera"===e&&"papel"===t?"win":"loss",pushToHistory(e,t){let n=this.getState(),a=this.whoWins(e,t);"win"===a&&n.history.player++,"loss"===a&&n.history.computer++,this.setState(n)},resetHistory(){let e=this.getState();e.history.player=0,e.history.computer=0,this.setState(e)}};var t={};t=import.meta.resolve("gyiAf");var n={};n=import.meta.resolve("bvizz");var a={};a=import.meta.resolve("ejkHH");var o={};o=import.meta.resolve("4wmXX");const r=[{path:/\/welcome/,component:function(e){let t=document.createElement("div");t.innerHTML=`
+const e={data:{currentGame:{playerPlay:"",computerPlay:""},history:{player:0,computer:0}},listeners:[],init(){},getState(){return this.data},setState(e){for(let t of(this.data=e,this.listeners))t();console.log("State updated:",this.data)},subscribe(e){this.listeners.push(e)},setPlayerMove(e){this.getState().currentGame.playerPlay=e,this.setComputerMove()},setComputerMove(){let e=["piedra","papel","tijera"][Math.floor(3*Math.random())],t=this.getState();t.currentGame.computerPlay=e,this.pushToHistory(t.currentGame.playerPlay,e)},whoWins:(e,t)=>e===t?"tie":"piedra"===e&&"tijera"===t||"papel"===e&&"piedra"===t||"tijera"===e&&"papel"===t?"win":"loss",pushToHistory(e,t){let n=this.getState(),a=this.whoWins(e,t);"win"===a&&n.history.player++,"loss"===a&&n.history.computer++,this.setState(n)},resetHistory(){let e=this.getState();e.history.player=0,e.history.computer=0,this.setState(e)}};var t={};t=import.meta.resolve("gyiAf");var n={};n=import.meta.resolve("bvizz");var a={};a=import.meta.resolve("ejkHH");const o=[{path:/\/welcome/,component:function(e){let t=document.createElement("div");t.innerHTML=`
     <h1 class="title">Piedra<br>Papel \xf3<br>Tijera</h1>
     <button-el class="start-button">Empezar</button-el>
     <div class="hands-container">
@@ -30,7 +30,7 @@ const e={data:{currentGame:{playerPlay:"",computerPlay:""},history:{player:0,com
       display: flex;
       gap: 40px;
     }
-  `,t.appendChild(n);let a=t.querySelector(".start-button");return a&&a.addEventListener("click",()=>{e.goTo("/instructions")}),t},background:`url(${o})`},{path:/\/instructions/,component:function(e){let t=document.createElement("div");t.innerHTML=`
+  `,t.appendChild(n);let a=t.querySelector(".start-button");return a&&a.addEventListener("click",()=>{e.goTo("/instructions")}),t}},{path:/\/instructions/,component:function(e){let t=document.createElement("div");t.innerHTML=`
     <p class="instructions-text">Presion\xe1 jugar y eleg\xed: piedra, papel o tijera antes de que pasen los 3 segundos.<br><br>El primero en llegar a 3 puntos gana el juego</p>
     <button-el class="play-button">\xa1Jugar!</button-el>
     <div class="hands-container">
@@ -62,7 +62,7 @@ const e={data:{currentGame:{playerPlay:"",computerPlay:""},history:{player:0,com
       display: flex;
       gap: 40px;
     }
-  `,t.appendChild(n);let a=t.querySelector(".play-button");return a&&a.addEventListener("click",()=>{e.goTo("/play")}),t},background:`url(${o})`},{path:/\/play/,component:function(t){let n=document.createElement("div"),a=3,o=()=>{let o=n.querySelector(".countdown-number");o&&(o.textContent=String(a)),--a<0&&(clearInterval(r),""===e.getState().currentGame.playerPlay&&e.setPlayerMove(""),t.goTo("/showdown"))},r=setInterval(o,1e3);n.innerHTML=`
+  `,t.appendChild(n);let a=t.querySelector(".play-button");return a&&a.addEventListener("click",()=>{e.goTo("/play")}),t}},{path:/\/play/,component:function(t){let n=document.createElement("div"),a=3,o=()=>{let o=n.querySelector(".countdown-number");o&&(o.textContent=String(a)),--a<0&&(clearInterval(r),""===e.getState().currentGame.playerPlay&&e.setPlayerMove(""),t.goTo("/showdown"))},r=setInterval(o,1e3);n.innerHTML=`
     <div class="play-container">
       <div class="countdown-container">
         <div class="countdown-number">3</div>
@@ -150,7 +150,7 @@ const e={data:{currentGame:{playerPlay:"",computerPlay:""},history:{player:0,com
     .hand:not(.selected) {
       opacity: 0.5;
     }
-  `,n.appendChild(i);let s=n.querySelectorAll(".hand");return s.forEach(n=>{n.addEventListener("click",()=>{let a=n.getAttribute("type");a&&(e.setPlayerMove(a),s.forEach(e=>e.classList.remove("selected")),n.classList.add("selected"),clearInterval(r),setTimeout(()=>{t.goTo("/showdown")},500))})}),o(),n},background:`url(${o})`},{path:/\/showdown/,component:function(t){let n=document.createElement("div"),a=e.getState(),o=a.currentGame.playerPlay,r=a.currentGame.computerPlay;n.innerHTML=`
+  `,n.appendChild(i);let s=n.querySelectorAll(".hand");return s.forEach(n=>{n.addEventListener("click",()=>{let a=n.getAttribute("type");a&&(e.setPlayerMove(a),s.forEach(e=>e.classList.remove("selected")),n.classList.add("selected"),clearInterval(r),setTimeout(()=>{t.goTo("/showdown")},500))})}),o(),n}},{path:/\/showdown/,component:function(t){let n=document.createElement("div"),a=e.getState(),o=a.currentGame.playerPlay,r=a.currentGame.computerPlay;n.innerHTML=`
     <div class="showdown-container">
       <hand-el type="${r}" class="computer-hand"></hand-el>
       <hand-el type="${o}" class="player-hand"></hand-el>
@@ -172,16 +172,16 @@ const e={data:{currentGame:{playerPlay:"",computerPlay:""},history:{player:0,com
     .computer-hand {
       transform: rotate(180deg);
     }
-  `,n.appendChild(i),setTimeout(()=>{"tie"===e.whoWins(o,r)?t.goTo("/play"):t.goTo("/result")},2e3),n},background:`url(${o})`},{path:/\/result/,component:function(o){let r=document.createElement("div"),i=e.getState(),s=i.history.player>=3,l=i.history.computer>=3,d="",c="Siguiente Ronda",p=!1,h="";s?(d=t,c="Jugar de Nuevo",p=!0,h="¡Ganaste la partida!"):l?(d=n,c="Jugar de Nuevo",p=!0,h="Perdiste la partida"):"win"===e.whoWins(i.currentGame.playerPlay,i.currentGame.computerPlay)?(d=t,h="Ganaste esta ronda"):(d=n,h="Perdiste esta ronda"),r.innerHTML=`
+  `,n.appendChild(i),setTimeout(()=>{"tie"===e.whoWins(o,r)?t.goTo("/play"):t.goTo("/result")},2e3),n}},{path:/\/result/,component:function(o){let r=document.createElement("div"),i=e.getState(),s=i.history.player>=3,l=i.history.computer>=3,c="",d="Siguiente Ronda",p=!1,h="";s?(c=t,d="Jugar de Nuevo",p=!0,h="¡Ganaste la partida!"):l?(c=n,d="Jugar de Nuevo",p=!0,h="Perdiste la partida"):"win"===e.whoWins(i.currentGame.playerPlay,i.currentGame.computerPlay)?(c=t,h="Ganaste esta ronda"):(c=n,h="Perdiste esta ronda"),r.innerHTML=`
     ${s&&p?`<img class="confetti" src="${a}" alt="confetti">`:""}
     <div class="result-container">
-      <img class="result-image" src="${d}" alt="${h}">
+      <img class="result-image" src="${c}" alt="${h}">
       <div class="score-board">
         <h2 class="score-title">Score</h2>
         <p class="score-p">Vos: ${i.history.player}</p>
         <p class="score-p">M\xe1quina: ${i.history.computer}</p>
       </div>
-      <button-el class="play-again-button">${c}</button-el>
+      <button-el class="play-again-button">${d}</button-el>
     </div>
   `;let u=document.createElement("style");u.innerHTML=`
     .result-container {
@@ -231,7 +231,7 @@ const e={data:{currentGame:{playerPlay:"",computerPlay:""},history:{player:0,com
       object-fit: cover;
       z-index: 1;
     }
-  `,r.appendChild(u);let m=r.querySelector(".play-again-button");return m&&m.addEventListener("click",()=>{p&&e.resetHistory(),o.goTo("/play")}),r},background:"solid"}];class i extends HTMLElement{constructor(){super(),this.render()}render(){let e=this.attachShadow({mode:"open"}),t=document.createElement("button"),n=document.createElement("style");t.textContent=this.textContent,n.innerHTML=`
+  `,r.appendChild(u);let m=r.querySelector(".play-again-button");return m&&m.addEventListener("click",()=>{p&&e.resetHistory(),o.goTo("/play")}),r},background:"solid"}];class r extends HTMLElement{constructor(){super(),this.render()}render(){let e=this.attachShadow({mode:"open"}),t=document.createElement("button"),n=document.createElement("style");t.textContent=this.textContent,n.innerHTML=`
       .button {
         width: 100%;
         height: 87px;
@@ -243,7 +243,7 @@ const e={data:{currentGame:{playerPlay:"",computerPlay:""},history:{player:0,com
         color: white;
         cursor: pointer;
       }
-    `,t.classList.add("button"),e.appendChild(n),e.appendChild(t)}}customElements.define("button-el",i);var s={};s=import.meta.resolve("9dVwf");var l={};l=import.meta.resolve("6kYrV");var d={};d=import.meta.resolve("6FgcY");class c extends HTMLElement{constructor(){super(),this.shadow=this.attachShadow({mode:"open"}),this.type=this.getAttribute("type")||"piedra",this.render()}render(){let e,t=document.createElement("style");"piedra"===this.type&&(e=s),"papel"===this.type&&(e=l),"tijera"===this.type&&(e=d),t.innerHTML=`
+    `,t.classList.add("button"),e.appendChild(n),e.appendChild(t)}}customElements.define("button-el",r);var i={};i=import.meta.resolve("9dVwf");var s={};s=import.meta.resolve("6kYrV");var l={};l=import.meta.resolve("6FgcY");class c extends HTMLElement{constructor(){super(),this.shadow=this.attachShadow({mode:"open"}),this.type=this.getAttribute("type")||"piedra",this.render()}render(){let e,t=document.createElement("style");"piedra"===this.type&&(e=i),"papel"===this.type&&(e=s),"tijera"===this.type&&(e=l),t.innerHTML=`
       .hand {
         width: var(--hand-width, 70px);
         height: var(--hand-height, 130px);
@@ -252,5 +252,5 @@ const e={data:{currentGame:{playerPlay:"",computerPlay:""},history:{player:0,com
         background-repeat: no-repeat;
         background-position: center;
       }
-    `,this.shadow.innerHTML='<div class="hand"></div>',this.shadow.appendChild(t)}}customElements.define("hand-el",c);const p=document.querySelector("#root");p&&(e.init(),function(t){let n="/gh-page-ppt";function a(){return location.host.includes("github.io")}function o(e){let t=a()?`${n}${e}`:e;history.pushState({},"",t),i(e)}function i(n){for(let a of(console.log(`Navigating to: ${n}`),r))if(a.path.test(n)){let n=a.component({goTo:o});if("solid"===a.background){let n=e.whoWins(e.getState().currentGame.playerPlay,e.getState().currentGame.computerPlay);"win"===n&&(t.style.backgroundColor="#888949"),"loss"===n&&(t.style.backgroundColor="#894949"),t.style.backgroundImage="none"}else t.style.backgroundImage=a.background,t.style.backgroundColor="transparent";t.firstChild&&t.firstChild.remove(),t.appendChild(n);return}o("/welcome")}function s(e){return a()&&e.startsWith(n)?e.substring(n.length)||"/":e}i(s(location.pathname)),window.onpopstate=()=>{i(s(location.pathname))}}(p));
-//# sourceMappingURL=PPT.f6f9f375.js.map
+    `,this.shadow.innerHTML='<div class="hand"></div>',this.shadow.appendChild(t)}}customElements.define("hand-el",c);const d=document.querySelector("#root");d&&(e.init(),function(t){let n="/gh-page-ppt";function a(){return location.host.includes("github.io")}function r(e){let t=a()?`${n}${e}`:e;history.pushState({},"",t),i(e)}function i(n){for(let a of(console.log(`Navigating to: ${n}`),o))if(a.path.test(n)){let n=a.component({goTo:r});if(t.style.backgroundImage="",t.style.backgroundColor="transparent","solid"===a.background){let n=e.whoWins(e.getState().currentGame.playerPlay,e.getState().currentGame.computerPlay);"win"===n&&(t.style.backgroundColor="#888949"),"loss"===n&&(t.style.backgroundColor="#894949")}t.firstChild&&t.firstChild.remove(),t.appendChild(n);return}r("/welcome")}function s(e){return a()&&e.startsWith(n)?e.substring(n.length)||"/":e}i(s(location.pathname)),window.onpopstate=()=>{i(s(location.pathname))}}(d));
+//# sourceMappingURL=PPT.ccfc03dd.js.map
